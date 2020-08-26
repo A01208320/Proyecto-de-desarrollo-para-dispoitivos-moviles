b23e0004c5540fb9a5c756acd0eeeb594a163b4c
A01208320 <A01208320@itesm.mx>
2020-08-26 14:00:21 -0600

    Ejercicio 1

---
+++ Ejercicios de Clase/Example1ITESMAD2020/app/src/main/java/com/example/example_1_itesm_ad2020/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.example_1_itesm_ad2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button myButton;
+    View myView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButton = findViewById(R.id.button);
+        myView = findViewById(R.id.text_view);
+    }
+}
