e008fa7af79c31a9223b123b799e80f9c8430e4f
A01208320 <A01208320@itesm.mx>
2020-10-27 12:40:24 -0600

    Firebase

--- Flutter/Oct15/votes_flutter/lib/main.dart
+++ Flutter/Oct15/votes_flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:votes_flutter/view/MyApp.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
--- Flutter/Oct15/votes_flutter/lib/view/MyApp.dart
+++ Flutter/Oct15/votes_flutter/lib/view/MyApp.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+
+  List<String> lista=['nombre1', 'nombre2', 'nombre3'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,8 +13,33 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Vote for a project'),
         ),
-        body: ListView(
-
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('Project').snapshots(),
+          builder: (context, snapshot){
+            if(!snapshot.hasData) return Text('Loading');
+            if(snapshot.data.documents.length==0){
+              return Center(
+              child: Text('No documents'),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (BuildContext context, int index){
+                DocumentSnapshot data =snapshot.data.documents(index);
+                return ListTile(
+                  title: Text(
+                    data['project_name'],
+                  ),
+                  subtitle: Text(
+                    data['author'],
+                  ),
+                  trailing: Text(
+                    data['votes'].toString(),
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
